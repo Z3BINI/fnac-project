55b0f0252870651b43ab4f95fcc526353186ef96
Eusébio Alves <eusebio.a95@hotmail.com>
2024-03-31 12:19:45 +0100

    First spell scene
    Tweaking code and still changing things for future modularity.

---
+++ scripts/spells/fire/basic_fire_ball.gd
@@ -0,0 +1,47 @@
+extends Spell
+
+@onready var do_damage_component = $DoDamageComponent
+@onready var animation_player = $AnimationPlayer
+
+var dir1 : Vector2  # Mouse
+var dir2 : Vector2  # Controller
+
+func _ready():
+	channel()
+	
+func _physics_process(delta):
+	if ready_to_throw and set_throw:
+		throw(delta)
+
+func channel():
+	animation_player.play("spawn")
+	await animation_player.animation_finished
+	ready_to_throw = true
+	animation_player.play("idle")
+	
+func cancel():
+	animation_player.play("impact")
+	await animation_player.animation_finished
+	queue_free()
+
+func throw(delta):
+	animation_player.play("throw")
+	global_position += dir1 * delta * THROW_SPEED
+
+func pre_throw():
+	rotation_degrees = rotate_towards_vector(dir1)  # Specificly situated here due to sprite in question
+	animation_player.play("pre_throw")
+	await animation_player.animation_finished
+
+func _on_do_damage_component_did_damage():
+	pass # Replace with function body.
+
+func _on_wall_detect_component_hit_wall():
+	pass # Replace with function body.
+
+func rotate_towards_vector(normalized_direction: Vector2):
+	# Calculate the angle between the normalized direction and the positive x-axis
+	var angle = atan2(normalized_direction.y, normalized_direction.x)
+	# Convert the angle to degrees and rotate the Node2D
+	return angle * 180 / PI
+	
