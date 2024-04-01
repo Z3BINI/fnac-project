2279ab8e04217c4b7e415f7d65b787a4c5fcff30
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-01 17:56:42 +0100

    Declaring spell types for handling
    To know for example where to instantiate

--- scripts/spells/spell.gd
+++ scripts/spells/spell.gd
@@ -1,6 +1,8 @@
 extends Node2D
 class_name Spell
 
+@export_enum('THROWABLE', 'SELF_INFLICTING') var SPELL_TYPE : String
+
 @export var CAST_TIME : float
 @export var MANA_COST : float
 @export var TRAVEL_SPEED : float
