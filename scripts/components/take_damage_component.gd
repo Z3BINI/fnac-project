869ddbabcbd9588adfe18cb30f49456db97c6256
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-01 16:49:31 +0100

    Took damage signal
    To animate shield when damage is absorved.

--- scripts/components/take_damage_component.gd
+++ scripts/components/take_damage_component.gd
@@ -1,6 +1,8 @@
 extends Area2D
 class_name TakeDamageComponent
 
+signal took_damage
+
 @export var HEALTH_COMPONENT : HealthComponent
 
 func can_take_damage() -> bool:  # Checks if we are alive to take the hit
@@ -8,4 +10,5 @@ func can_take_damage() -> bool:  # Checks if we are alive to take the hit
 	
 func _on_area_entered(area : DoDamageComponent):
 	if can_take_damage():
+		took_damage.emit()
 		HEALTH_COMPONENT.take_damage(area.DAMAGE_AMOUNT)
