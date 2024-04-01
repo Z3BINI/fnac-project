5535a85a0e619022fc2095b114d094172f2708b3
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-01 10:52:41 +0100

    Added some basic features to do damage
    Signals when damage occurs for parent node to take care of itself on impact. Also, toggle collider func to turn on only after spell is channeled.

--- scripts/components/do_damage_component.gd
+++ scripts/components/do_damage_component.gd
@@ -1,4 +1,13 @@
 extends Area2D
 class_name DoDamageComponent
 
-var DAMAGE_AMOUNT : float
+signal did_damage
+
+@export var DAMAGE_AMOUNT : float
+
+func _on_area_entered(area : HealthComponent):
+	area.take_damage(DAMAGE_AMOUNT)
+	did_damage.emit()
+
+func toggle_detection_disabled(state : bool):
+	get_child(0).disabled = state
