6c6301e890b18c2bbc1212395fb8c1640591c355
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-02 18:25:52 +0100

    Swapped all health related signals do health component

--- scripts/components/health_component.gd
+++ scripts/components/health_component.gd
@@ -2,6 +2,8 @@ extends Node
 class_name HealthComponent
 
 signal died
+signal took_damage
+signal healed
 
 @export var MAX_HP : float = 100
 
@@ -12,10 +14,13 @@ func _ready() -> void:
 
 func heal(heal_amount : float) -> void:
 	current_hp += heal_amount
+	healed.emit()
 	if current_hp > MAX_HP: current_hp = MAX_HP
 
 func take_damage(damage_amount : float) -> void: 
 	current_hp -= damage_amount
+
+	took_damage.emit()
 	
 	if current_hp <= 0:
 		died.emit()
--- scripts/components/take_damage_component.gd
+++ scripts/components/take_damage_component.gd
@@ -1,8 +1,6 @@
 extends Area2D
 class_name TakeDamageComponent
 
-signal took_damage
-
 @export var HEALTH_COMPONENT : HealthComponent
 
 func can_take_damage() -> bool:  # Checks if we are alive to take the hit
@@ -10,5 +8,4 @@ func can_take_damage() -> bool:  # Checks if we are alive to take the hit
 	
 func _on_area_entered(area : DoDamageComponent):
 	if can_take_damage():
-		took_damage.emit()
 		HEALTH_COMPONENT.take_damage(area.DAMAGE_AMOUNT)
