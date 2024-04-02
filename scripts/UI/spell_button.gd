608a80d8ab83d26df3e4e49ef9d0f0ffba911d60
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-02 15:08:22 +0100

    Spell button tweaking
    Removed key change feature, won't be necessary for this project. Added on_cd variable to help ability manager know if can cast. Set spell stats func to set properties of any spell loaded to this button.

--- scripts/UI/spell_button.gd
+++ scripts/UI/spell_button.gd
@@ -1,44 +1,38 @@
 extends TextureButton
 
-@onready var cooldown = $Cooldown
-@onready var key = $Key
+@onready var cooldown_texture = $Cooldown
 @onready var time = $Time
 @onready var timer = $Timer
+@onready var key = $Key
 
 var spell : Spell = null
 
-var change_key = "":
-	set(value):
-		change_key = value
-		key.text = value
-		
-		shortcut = Shortcut.new()
-		var new_key = InputEventKey.new()
-		new_key.keycode = value.unicode_at(0)
-		
-		shortcut.events = [new_key]
+var on_cd : bool = false
 
 func _ready():
-	change_key = "Q"
-	cooldown.max_value = timer.wait_time
+	if spell != null:
+		set_spell_stats()
+
+func set_spell_stats():
+	timer.wait_time = spell.COOLDOWN
+	time.text = str(spell.COOLDOWN)
+	
+	texture_normal = spell.TEXTURE
+	
+	cooldown_texture.max_value = timer.wait_time
+	cooldown_texture.value = 0
 	set_process(false)
 
 func _process(_delta):
 	time.text = "%3.1f" % timer.time_left
-	cooldown.value = timer.time_left
+	cooldown_texture.value = timer.time_left
 
-
-func _on_pressed():
-	if spell != null:
-		spell.cast()
-		
-		timer.start()
-		disabled = true
-		set_process(true)
-
+func used_spell():
+	on_cd = true
+	timer.start()
+	set_process(true)
 
 func _on_timer_timeout():
-	disabled = false
-	time.text = ""
-	cooldown.value = 0
+	on_cd = false
+	set_spell_stats()
 	set_process(false)
