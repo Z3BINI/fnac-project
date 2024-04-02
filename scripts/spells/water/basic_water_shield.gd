213f07d92c6df9712e98e4a47bbff9f31d4da86d
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-02 18:24:19 +0100

    Water shield logic
    Reduce its size during use and defense. Active bool to keep track of current size when not fully depleted.

--- scripts/spells/water/basic_water_shield.gd
+++ scripts/spells/water/basic_water_shield.gd
@@ -1,8 +1,23 @@
 extends Spell
 
+@export var REDUCER : float = 0.15
 
+var active : bool = false
+var store_scale : Vector2 = Vector2.ZERO
+
+func _ready():
+	$TakeDamageComponent.HEALTH_COMPONENT = $HealthComponent  # Wasnt auto loading on instances
+
+func _physics_process(delta):
+	if active == true:
+		scale -= Vector2(REDUCER, REDUCER) * delta
+		
 func cast():
 	pass
 	
 func cancel():
+	active = false
 	queue_free()
+
+func _on_health_component_took_damage():
+	scale -= Vector2(0.3, 0.3)
