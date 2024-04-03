25ce1e2a9b456cbfe4164965614975d49cfa4720
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-03 16:03:34 +0100

    Removed self inflicting damage bugg

--- scripts/components/take_damage_component.gd
+++ scripts/components/take_damage_component.gd
@@ -9,7 +9,9 @@ func can_take_damage() -> bool:  # Checks if we are alive to take the hit
 	return (HEALTH_COMPONENT.current_hp > 0)
 	
 func _on_area_entered(area : DoDamageComponent):
-	if can_take_damage():
+	if (can_take_damage() and 
+	(area.get_parent().name != get_parent().name) and 
+	!(area.get_parent().is_in_group("player_ability") and get_parent().is_in_group("player"))):
 		HEALTH_COMPONENT.take_damage(area.DAMAGE_AMOUNT)
 
 func toggle_take_damage_disabled(state):
