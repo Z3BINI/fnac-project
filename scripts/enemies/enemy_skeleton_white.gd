7ce5ff2ad204ee77aadb69c237fc3357a87d5119
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-03 15:37:12 +0100

    Takes care of moving itself

---
+++ scripts/enemies/enemy_skeleton_white.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+var current_direction : Vector2
+var current_speed : float 
+var current_accel : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if velocity != Vector2.ZERO: 
+		$Sprite2D.flip_h = (velocity.x < 0)
+	
+	# Temporary logic for flipping damage box with this sprite
+	if ($Sprite2D.flip_h): $DoDamageComponent.rotation_degrees = 180
+	else: $DoDamageComponent.rotation_degrees = 0
+
+func _physics_process(_delta):
+	velocity = velocity.move_toward(current_direction * current_speed, current_accel)
+	move_and_slide()
