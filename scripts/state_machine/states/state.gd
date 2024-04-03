75cf2961883ed6176421f050b0d69e0eab33ae01
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-03 15:32:04 +0100

    Going to send player body with signal
    Instead of having the player body known all the time, only send in from signal when relevant.

--- scripts/state_machine/states/state.gd
+++ scripts/state_machine/states/state.gd
@@ -2,10 +2,11 @@ extends Node
 class_name State
 
 @export var self_body : CharacterBody2D
+@export var animation_player : AnimationPlayer
 
 signal state_changed
 
-func enter_state():
+func enter_state(player_body : Player = null):
 	pass
 	
 func exit_state():
