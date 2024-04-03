89e44c825e9111d7d52e6bc100408f0885c88898
Eusébio Alves <eusebio.a95@hotmail.com>
2024-04-03 15:35:13 +0100

    Efficiency
    Like roam setting speed settings once on state enter and listening to area signal for hostile state.

--- scripts/state_machine/states/enemy_states/idle.gd
+++ scripts/state_machine/states/enemy_states/idle.gd
@@ -10,19 +10,22 @@ var idle_time : float
 func randomize_idle_time():
 	idle_time = randf_range(MIN_IDLE_TIME, MAX_IDLE_TIME)
 
-func enter_state():
+func enter_state(player_body : Player = null):
 	randomize_idle_time()
+	if self_body:
+		self_body.current_speed = 0.0
+		self_body.current_direction = Vector2.ZERO
+		
+		animation_player.play("idle")
 
 func update(delta):
-	if self_body.player_in_range and !self_body.PLAYER.dead:
-		state_changed.emit(self, 'hostile')
+	#if self_body.player_in_range and !self_body.PLAYER.dead:
+		#state_changed.emit(self, 'hostile')
 	
 	if idle_time > 0:
 		idle_time -= delta
 	else:
-		state_changed.emit(self, 'roam')
+		state_changed.emit(self, 'roam', null)
 
-func physics_update(_delta):
-	if self_body:
-		self_body.velocity = Vector2.ZERO
-
+func _on_player_detection_body_entered(body):
+	state_changed.emit(self, 'hostile', body)
